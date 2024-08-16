3ec857c804a160cf996ef4b2719edb8991bf3e17
under_world <underworld.programmer@gmail.com>
2024-08-15 21:28:46 -0500

    Profile photo added

--- src/components/ProfileNav/ProfileNav.jsx
+++ src/components/ProfileNav/ProfileNav.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import './profileNav.css';
 import ExitIcon from "./../../assets/exit_icon.svg";
+import { useNavigate } from 'react-router-dom';
+import DefProfileImg from "./../../assets/honey.png"; // "./../../assets/blank-profile-picture.svg";
 
-const ProfileNav = ({user}) => {
+const ProfileNav = ({ user }) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -12,13 +16,15 @@ const ProfileNav = ({user}) => {
 
   const logout = () => {
     localStorage.removeItem('user'); 
-    window.location.href = '/';
+    navigate('/');
+    location.reload();
   };
 
   return (
     <div className="profile">
       <button onClick={toggleDropdown} className='amarillo'>
-        @{ user.name } v
+        <div className='username'>@{ user.name }</div>
+        <div className='right-container'><img className="profile-img" src={(user.image == null || user.image === '') ? DefProfileImg: user.image} alt="" /><span>v</span> </div>
       </button>
       {isOpen && (
         <ul className="drop" style={{listStyleType: 'none'}}>
