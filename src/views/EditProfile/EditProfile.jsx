f70d81c673c53730fe32112a259b4de24c14e9e2
under_world <underworld.programmer@gmail.com>
2024-08-17 22:25:49 -0500

    some things

--- src/views/EditProfile/EditProfile.jsx
+++ src/views/EditProfile/EditProfile.jsx
@@ -2,6 +2,7 @@ import './editProfile.css'
 import Nav from '../../components/Nav/Nav'
 import { useEffect, useState } from 'react';
 import DefaultImg from '../../assets/honey.png';
+import apiService from '../../api/apiService';
 
 const EditProfile = ( ) => {
 
@@ -13,11 +14,28 @@ const EditProfile = ( ) => {
 		password: '',
 		image: '',
 		publicKey: ''
-
 	});
+	const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+		image: '',
+		publicKey: ''
+  });
+
+	const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+	}
 
 	useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user'));
+		if ( user.image != null || user.image != "") {
+			setImagePreview(user.image)
+		}
     // Do something with the user
 		setUser(user);	
 	}, []);
@@ -49,23 +67,82 @@ const handleImageUpload = (event) => {
 	}
 };
 
+const handleSubmit = (e) => {
+	e.preventDefault();
+
+	if (formData.name.length > 0) {
+		user.name = formData.name;
+	}
+	if (formData.email.length > 0) {
+		user.email = formData.email;
+	}
+	if (formData.password.length > 0) {
+		user.password = formData.password;
+	}
+	if (formData.publicKey.length > 0) {
+		user.publicKey = formData.publicKey;
+	}
+
+
+
+	const user_id = JSON.parse(localStorage.getItem("user")).id;
+
+
+	// Subir la imagen si se seleccionó una
+	if (imageFile) {
+		const imageFormData = new FormData();
+		imageFormData.append("file", imageFile);
+
+		apiService.uploadProfileImage(user_id, imageFormData)
+			.then(imageUrl => {
+				console.log('Imagen subida:', imageUrl);
+				user.image = imageUrl; // Agrega la URL de la imagen al objeto user
+				return apiService.updateUser(user_id, user);
+			})
+			.then(() => {
+				console.log('Usuario actualizado');
+				setUser(user);
+				localStorage.setItem('user', JSON.stringify(user));
+				alert('Usuario actualizado exitosamente');
+				window.location.reload();
+			})
+			.catch(err => {
+				console.error('Error:', err);
+				alert('Error al actualizar el usuario');
+			});
+	} else {
+		apiService.updateUser(user_id, user)
+			.then(() => {
+				console.log('Usuario actualizado');
+				localStorage.setItem('user', JSON.stringify(user));
+				alert('Usuario actualizado exitosamente');
+				window.location.reload();
+			})
+			.catch(err => {
+				console.error('Error:', err);
+				alert('Error al actualizar el usuario');
+			});
+	}
+};
+
+
 	return (
 		<>
 			<Nav />
 			<div className='edit-profile-container'>
 				<div className='edit-profile-form'>
 					<h2>Profile Settings</h2>
-					<form action="">
+					<form onSubmit={handleSubmit}>
 						<div className='input-form'>
-						<label htmlFor="name">Name: </label> <input type="text" name="name" id="name" placeholder={user.name.length > 0 ? user.name : "Edit your name"}/>
+						<label htmlFor="name">Name: </label> <input onChange={handleChange} type="text" name="name" id="name" placeholder={user.name.length > 0 ? user.name : "Edit your name"}/>
 						</div>
 
 						<div className='input-form'>
-						<label htmlFor="email">Email: </label> <input type="mail" name="email" id="email" placeholder={user.email.length > 0 ? user.email : "Edit your email	"}/>
+						<label htmlFor="email">Email: </label> <input onChange={handleChange} type="mail" name="email" id="email" placeholder={user.email.length > 0 ? user.email : "Edit your email	"}/>
 						</div>
 
 						<div className='input-form'>
-						<label htmlFor="password">Password: </label> <input type="password" name="password" id="password" placeholder='Enter a new password'/>
+						<label htmlFor="password">Password: </label> <input onChange={handleChange} type="password" name="password" id="password" placeholder='Enter a new password'/>
 						</div>
 
 						<div className='input-form'>
@@ -74,10 +151,10 @@ const handleImageUpload = (event) => {
 						<span>select file</span>
 							
 							</label> 
-							<input type="file" name="image" id="image" accept="image/*" onChange={handleImageUpload}/>
+							<input  type="file" name="image" id="image" accept="image/*" onChange={handleImageUpload}/>
 						</div>
 						<div className='input-form wallet-input'>
-						<label htmlFor="wallet">Wallet-id: </label> <input type="text" name="wallet" id="wallet" placeholder={user.publicKey == null ||user.publicKey.length < 0 ? "Edit your wallet	": user.publicKey }/>
+						<label htmlFor="wallet">Wallet-id: </label> <input onChange={handleChange} type="text" name="publicKey" id="wallet" placeholder={user.publicKey == null ||user.publicKey.length < 0 ? "Edit your wallet	": user.publicKey }/>
 						</div>
 						<button className='btn-form'>Save</button>
 					</form>
