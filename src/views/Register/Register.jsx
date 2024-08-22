b70706e1cd74ae250dec1fdeed373c8e77fa3df9
under_world <underworld.programmer@gmail.com>
2024-08-22 17:51:43 -0500

    bug fixed

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -90,7 +90,7 @@ function Register() {
             password,
             type: userType,
             image: '',
-            publicKey: ''
+            publicKey: null,
         };
 
         try {
