d71b3eb292b3c9670bfbf42ca6635af23d6b3e7a
Melany <mefrancom@unal.edu.co>
2025-05-03 13:20:04 -0500

    actualizacion api

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8080/api/v1'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const apiService = {
   login: (credentials) => axios.post(`${API_BASE_URL}/login`, credentials),
